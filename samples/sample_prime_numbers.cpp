ba5512e3e65c5c6ffda504a82f31f4a996b5f5aa
Кристина крымова <krymova_kristina@mail.ru>
2023-09-18 15:39:04 +0300

    my first commit

--- samples/sample_prime_numbers.cpp
+++ samples/sample_prime_numbers.cpp
@@ -16,7 +16,7 @@
 
 int main()
 {
-  int n, m, k, count;
+    /*int n, m, k, count;
 
   setlocale(LC_ALL, "Russian");
   cout << "Тестирование программ поддержки битового поля" << endl;
@@ -48,7 +48,8 @@ int main()
         cout << endl;
     }
   cout << endl;
-  cout << "В первых " << n << " числах " << count << " простых" << endl;
+
+  cout << "В первых " << n << " числах " << count << " простых" << endl;*/
 }
 #else
 
